2c4e2f8a8d2f1f85f97fcb476b052bf8bbbc8a39
Diego Ramos <diegorbrito9@gmail.com>
2023-04-23 18:07:07 -0300

    [ignite] [reactjs] [2021] [fundamentos next.Js] [estrutura da aplicação] fim da aula 'configurando fonte externa'

--- fundamentos-nextjs/src/pages/index.tsx
+++ fundamentos-nextjs/src/pages/index.tsx
@@ -1,5 +1,13 @@
 import styles from "@/styles/pages/home.module.scss";
+import Head from "next/head";
 
 export default function Home() {
-  return <h1 className={styles.title}>Home</h1>;
+  return (
+    <>
+      <Head>
+        <title>ig.news</title>
+      </Head>
+      <h1 className={styles.title}>Home</h1>
+    </>
+  );
 }
